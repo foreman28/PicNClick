97633eafa0b9d2bdb5fb8dca4eb8fe5d51677a5e
foreman28 <daniilanisimov2004@gmail.com>
2023-11-19 23:31:00 +0500

    start 3.7

---
+++ prisma/migrations/20231119181647_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorAvatar` on the `ForumPost` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ForumPost" DROP COLUMN "authorAvatar";
