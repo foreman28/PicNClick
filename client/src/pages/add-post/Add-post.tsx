5ddbeb5b91a36e08fa2043b5cc81c913117c5d06
foreman28 <daniilanisimov2004@gmail.com>
2023-11-30 17:37:22 +0500

    7.5 (I did the file upload!)

--- client/src/pages/add-post/Add-post.tsx
+++ client/src/pages/add-post/Add-post.tsx
@@ -1,12 +1,12 @@
 import {ChangeEvent, useEffect, useState} from 'react';
 import {Button, ConfigProvider, Flex, Form, message, Upload} from 'antd';
-import { Layout } from '../../components/layout/layout';
-import { useAddPostMutation } from '../../api/posts';
+import {Layout} from '../../components/layout/layout';
+import {useAddPostMutation} from '../../api/posts';
 import CustomBreadcrumb from '../../components/custom-breadcrumb/custom-breadcrumb';
-import { CustomInput } from '../../components/custom-input/custom-input';
-import { button } from '../../themes/buttons';
+import {CustomInput} from '../../components/custom-input/custom-input';
+import {button} from '../../themes/buttons';
 import styles from './Add-post.module.scss';
-import { CustomTextarea } from '../../components/custom-textarea/custom-textarea';
+import {CustomTextarea} from '../../components/custom-textarea/custom-textarea';
 // import CustomSelect from '../../components/custom-select/custom-select';
 import {DownloadOutlined} from "@ant-design/icons";
 
@@ -15,33 +15,18 @@ export const AddPost = () => {
     window.scrollTo(0, 0);
   }, []);
   
-  const [addPost, { isLoading }]:any = useAddPostMutation();
-  const [image, setImage]:any = useState('');
-
-  // const normFile = (e:any) => {
-  //   const fileList = e && e.fileList;
-  //   if (fileList && fileList.length > 0) {
-  //     getImage(fileList[0].originFileObj, (e:any) => {
-  //       setImage(e);
-  //     });
-  //   }
-  //   return fileList;
-  // };
-
-  // const getImage = (file:any, callback:any) => {
-  //   const reader = new FileReader();
-  //   reader.addEventListener('load', () => callback(reader.result));
-  //   reader.readAsDataURL(file);
-  // };
+  const [addPost, {isLoading}]: any = useAddPostMutation();
+  const [imageFile, setImageFile]: any = useState('');
+  const [showImage, setShowImage]: any = useState('');
   
   const onFinish = async (values: any) => {
     try {
-      // console.log(values.file)
+      console.log(imageFile)
       const formData = new FormData();
       formData.append('title', values.title);
       formData.append('description', values.description);
       formData.append('content', values.content);
-      formData.append('image', image);
+      formData.append('image', imageFile);
       
       await addPost(formData);
       message.success('Пост успешно добавлен!');
@@ -50,64 +35,88 @@ export const AddPost = () => {
       message.error('Произошла ошибка при добавлении поста.');
     }
   };
-
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      setImage(e.target.files[0]);
+  
+  const normFile = (e: any) => {
+    if (Array.isArray(e)) {
+      return e;
     }
+    return e && e.fileList;
   };
   
   const handleImageClick = () => {
     // @ts-ignore
-    document.getElementById("file").click();
+    document.getElementById("addFile").click();
   };
   
+  const handleFileChange = async (file: any) => {
+    try {
+      setImageFile(file);
+      
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setShowImage(e.target?.result);
+      };
+      reader.readAsDataURL(file);
+    } catch (error) {
+      console.error('Error reading file:', error);
+    }
+  };
+
   return (
     <Layout>
       <Flex vertical gap={12}>
-        <CustomBreadcrumb />
+        <CustomBreadcrumb/>
         <Form className={styles.item} layout="vertical" onFinish={onFinish}>
           <Flex vertical gap={4}>
-          <CustomInput name="title" placeholder="Заголовок" />
-          <CustomInput name="description" placeholder="Краткое описание" />
-
-          <div style={{display: "flex"}} onClick={handleImageClick}>
-            {image ? (
-              <img
-                src={image}
-                alt="Загруженное изображение"
-                className={styles.img}
-              />
-            ) : (
-              <div className={styles.img}>
-                <Flex vertical gap={4} align={"center"} justify={"center"} style={{ height: '100%' }}>
-                  <DownloadOutlined className={styles.icon} />
-                  <span className={styles.download}>Загрузить изображение</span>
-                </Flex>
-              </div>
-            )}
-          </div>
+            <CustomInput name="title" placeholder="Заголовок"/>
+            <CustomInput name="description" placeholder="Краткое описание"/>
             
-
-          <Form.Item
-            name="file"
-            // valuePropName="fileList"
-            // getValueFromEvent={normFile}
-            rules={[{ required: true, message: 'Обязательное поле' }]}>
-
-              <input type="file" name="file" id="file"  onChange={handleFileChange}/>
-
-          </Form.Item>
-          
-          <CustomTextarea name="content" />
-          
-          {/*<CustomSelect name="tags" placeholder="Теги" />*/}
-          
-          <ConfigProvider theme={button}>
-            <Button type="primary" htmlType="submit" loading={isLoading}>
-              Добавить пост
-            </Button>
-          </ConfigProvider>
+            <div style={{display: "flex"}} onClick={handleImageClick}>
+              {showImage ? (
+                <img
+                  src={showImage}
+                  alt="Загруженное изображение"
+                  className={styles.img}
+                />
+              ) : (
+                <div className={styles.img}>
+                  <Flex vertical gap={4} align={"center"} justify={"center"} style={{height: '100%'}}>
+                    <DownloadOutlined className={styles.icon}/>
+                    <span className={styles.download}>Загрузить изображение</span>
+                  </Flex>
+                </div>
+              )}
+            </div>
+            
+            
+            <Form.Item
+              name="file"
+              valuePropName="fileList"
+              getValueFromEvent={normFile}
+              rules={[{ required: true, message: 'Обязательное поле' }]}
+              style={{display: "none"}}
+            >
+              <Upload
+                id={"addFile"}
+                customRequest={() => {}}
+                beforeUpload={handleFileChange}
+                listType="picture"
+                maxCount={1}
+                accept="image/*"
+              >
+                <Button>Выберите файл</Button>
+              </Upload>
+            </Form.Item>
+            
+            <CustomTextarea name="content"/>
+            
+            {/*<CustomSelect name="tags" placeholder="Теги" />*/}
+            
+            <ConfigProvider theme={button}>
+              <Button type="primary" htmlType="submit" loading={isLoading}>
+                Добавить пост
+              </Button>
+            </ConfigProvider>
           </Flex>
         </Form>
       </Flex>
--- controllers/posts.js
+++ controllers/posts.js
@@ -101,9 +101,9 @@ const add = async (req, res) => {
     const data = req.body;
     const file = req.file; // Access the file information using req.file
 
-    // console.log(req);
+    console.log(data);
     console.log(file);
-    //
+
     if (!data.title || !data.content || !data.description) {
       return res.status(400).json({message: "Все поля обязательные"});
     }
